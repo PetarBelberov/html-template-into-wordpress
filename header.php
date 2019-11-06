3b4679e8f6efab8c7ddeafc671cb44fc6c1a823b
PetarBelberov <petar_belberov@gmx.com>
2019-11-06 15:40:32 +0200

    modifying header.php, styling

--- header.php
+++ header.php
@@ -1,11 +1,20 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package html-template-into-wordpress
+ */
 
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta charset="<?php bloginfo( 'description' ); ?>">
-
-
+    <link rel="profile" href="https://gmpg.org/xfn/11">
     <?php wp_head(); ?>
 </head>
 
